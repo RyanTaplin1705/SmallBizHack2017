3bb1b0094c43d03579587edc3db02b7aeb87ea8d
Thomas Heslin <tjheslin1@gmail.com>
2017-03-04 19:18:01 +0000

    Update stock working

--- src/main/java/io/github/tjheslin1/stock/StockQuery.java
+++ src/main/java/io/github/tjheslin1/stock/StockQuery.java
@@ -9,36 +9,19 @@ import com.intuit.ipp.query.GenerateQuery;
 import com.intuit.ipp.security.OAuthAuthorizer;
 import com.intuit.ipp.services.DataService;
 import com.intuit.ipp.services.QueryResult;
-import io.github.tjheslin1.http.HttpClient;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import static com.intuit.ipp.data.ItemTypeEnum.INVENTORY;
 import static com.intuit.ipp.query.GenerateQuery.$;
 import static com.intuit.ipp.query.GenerateQuery.select;
+import static java.lang.String.format;
 
 public class StockQuery {
 
-    private final HttpClient httpClient;
-
-    public StockQuery(HttpClient httpClient) {
-        this.httpClient = httpClient;
-    }
-
-    public void query(String name) throws IOException, FMSException {
-        OAuthAuthorizer oAuthAuthorizer = new OAuthAuthorizer(
-                "qyprdUqAVqEdkSGT27rAQTiLastMk4",
-                "1aWKvxa0IHkrUCTrJaEsl2xZjEhvRjKkIru0lmYZ",
-                "lvprdrchtljYI8IZ0L1GYr6bSk5gZJHTAJCJ1RRsm68qVRvj",
-                "aklvdVxVn0NKxa127VskV7c7foShMIszzG34oNWZ");
-
-        Context context = new Context(
-                oAuthAuthorizer,
-                "b29723abb9eedb4b0eb95c4b4fee36d39dd0",
-                ServiceType.QBO,
-                "123145730638514");
-
-        DataService dataService = new DataService(context);
+    public static void getItems() throws IOException, FMSException {
+        DataService dataService = dataService();
 
         Item item = GenerateQuery.createQueryEntity(Item.class);
 
@@ -53,24 +36,63 @@ public class StockQuery {
         }
     }
 
-//    public void query(String name) throws IOException {
-//        Response response = httpClient.execute(new Request(
-//                "https://sandbox-quickbooks.api.intuit.com/v3/company/123145730638514/query",
-//                "GET",
-//                "",
-//                asList(queryParameter("query", "SELECT * FROM item WHERE type = 'Inventory' AND name = 'Blue Navy Shirt - Large'")),
-//                asList(
-//                        header("Accept", "application/json"),
-//                        header("Authorization",
-//                                "OAuth oauth_token=\"b29723abb9eedb4b0eb95c4b4fee36d39dd0\"," +
-//                                        "oauth_nonce=\"46906266-ecad-4330-b38c-74a23c1c611b\"," +
-//                                        "oauth_consumer_key=\"qyprdUqAVqEdkSGT27rAQTiLastMk4\"," +
-//                                        "oauth_signature_method=\"HMAC-SHA1\"," +
-//                                        "oauth_timestamp=\"1488639751\"," +
-//                                        "oauth_version=\"1.0\"," +
-//                                        "oauth_signature=\"rCAINJqfiyAiR1WlTm52Dc5DE84%3D\"")
-//                )));
-//
-//        System.out.println(response.body);
-//    }
+    public static void updateItemStock(String itemName) {
+        DataService dataService = dataService();
+
+        Item item = getItem(itemName);
+        BigDecimal stockLevel = item.getQtyOnHand();
+
+        BigDecimal updatedStock = stockLevel.subtract(BigDecimal.ONE);
+        item.setQtyOnHand(updatedStock);
+
+        try {
+            dataService.update(item);
+        } catch (FMSException e) {
+            e.printStackTrace();
+            throw new IllegalStateException(format("Unable to update item '%s'", item.getName()));
+        }
+    }
+
+    private static Item getItem(String itemName) {
+        DataService dataService = dataService();
+
+        Item item = GenerateQuery.createQueryEntity(Item.class);
+
+        String query = select($(item)).where($(item.getType()).eq(INVENTORY)).where($(item.getName()).eq(itemName)).generate();
+
+        System.out.println("\n" + query + "\n");
+
+        QueryResult queryResult = null;
+        try {
+            queryResult = dataService.executeQuery(query);
+        } catch (FMSException e) {
+            throw new IllegalStateException(format("Unable to execute query '%s'", query));
+        }
+
+        Item foundItem = (Item) queryResult.getEntities().get(0);
+        System.out.println(foundItem.getName());
+
+        return foundItem;
+    }
+
+    private static DataService dataService() {
+        Context context;
+        try {
+            OAuthAuthorizer oAuthAuthorizer = new OAuthAuthorizer(
+                    "qyprdUqAVqEdkSGT27rAQTiLastMk4",
+                    "1aWKvxa0IHkrUCTrJaEsl2xZjEhvRjKkIru0lmYZ",
+                    "lvprdrchtljYI8IZ0L1GYr6bSk5gZJHTAJCJ1RRsm68qVRvj",
+                    "aklvdVxVn0NKxa127VskV7c7foShMIszzG34oNWZ");
+
+            context = new Context(
+                    oAuthAuthorizer,
+                    "b29723abb9eedb4b0eb95c4b4fee36d39dd0",
+                    ServiceType.QBO,
+                    "123145730638514");
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to set up context using OAuth.");
+        }
+
+        return new DataService(context);
+    }
 }
--- src/test/java/io/github/tjheslin1/stock/StockQueryTest.java
+++ src/test/java/io/github/tjheslin1/stock/StockQueryTest.java
@@ -1,23 +1,19 @@
 package io.github.tjheslin1.stock;
 
-import io.github.tjheslin1.http.ApacheHttpClient;
 import org.junit.Ignore;
 import org.junit.Test;
-
-import java.io.IOException;
 
 public class StockQueryTest {
 
     @Ignore
     @Test
     public void query() throws Exception {
-        StockQuery stockQuery = new StockQuery(new ApacheHttpClient());
-
-        try {
-            stockQuery.query("Blue Navy Shirt - Large");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        StockQuery.getItems();
     }
 
+    @Ignore
+    @Test
+    public void updateItem() throws Exception {
+        StockQuery.updateItemStock("Blue Navy Shirt");
+    }
 }
